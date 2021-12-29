65ac0f1a80f2834eff72b855257cdc25564bc3b1
Roberto Salinas <roberto.salinas@innocv.com>
2021-12-29 15:17:45 +0100

    created counter app

---
+++ src/components/Counter.vue
@@ -0,0 +1,53 @@
+<template>
+  <h2>{{ getTitle }}</h2>
+  <p> {{ counter }} <sup>2</sup> = {{squareCounter}} </p>
+  <div>
+      <button v-on:click="increaseCounter">+1</button>
+      <button @click="decreaseCounter">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        start: {
+            type: Number,
+            //required: true,
+            default: 1,
+            validator(value) {
+                return value > 0
+            }
+        }
+    },
+    data() {
+        return {
+            counter: this.start
+        }
+    },
+    methods: {
+        increaseCounter() {
+            this.counter++
+        },
+        decreaseCounter() {
+            this.counter--
+        }
+    },
+    computed: {
+        squareCounter() {
+            return this.counter * this.counter
+        },
+        getTitle() {
+            return this.title || 'Default title'
+        }
+    }
+}
+</script>
+
+<style>
+
+button {
+    margin: 0 5px;
+}
+
+</style>
